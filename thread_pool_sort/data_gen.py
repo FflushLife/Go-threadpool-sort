108b69eec0f88f1c784fa6910d786090bf0bc193
mike <mikelka150@gmail.com>
2019-12-23 02:18:52 +0300

    almost working randorm generated data

--- thread_pool_sort/data_gen.py
+++ thread_pool_sort/data_gen.py
@@ -2,7 +2,8 @@
 
 import random
 
-size = random.randint(1, 1000000)
+r_size = 1000000
+size = random.randint(1, 100)
 sample = [str(x) for x in random.sample(list(range(1, size)), size-1)]
 with open("rand_data.txt", "w+") as out:
     out.write(','.join(sample))
--- thread_pool_sort/src/main.go
+++ thread_pool_sort/src/main.go
@@ -13,9 +13,9 @@ import (
 	"unsafe"
 )
 
-func createTasks() [][]uint64{
+func createTasks() [][]int {
 	data_in := make([]byte, 8192*64)
-	var tasks [][]uint64
+	var tasks [][]int
 
 	file, err := os.Open("rand_data.txt")
 	defer file.Close()
@@ -24,18 +24,18 @@ func createTasks() [][]uint64{
 		panic(err)
 	}
 
-	_, err = file.Read(data_in)
+	count_bytes, err := file.Read(data_in)
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
-	data := strings.Split(string(data_in), "\n")
+	data := strings.Split(string(data_in[:count_bytes-1]), "\n")
 
 	for _, el := range data {
-		var task_single []uint64
+		var task_single []int
 		numbers_str := strings.Split(el, ",")
 		for _, number_str := range numbers_str {
-			number_64, err := strconv.ParseUint(number_str, 10, 64)
+			number_64, err := strconv.Atoi(number_str)
 			if err != nil {
 				log.Fatal(err)
 				panic(err)
@@ -49,10 +49,10 @@ func createTasks() [][]uint64{
 }
 
 func main() {
-	fmt.Println(createTasks())
 	var sortInstance *psort.PSort
 	var poolInstance *pool.Pool
 	var tCount uint64 = 1
+	var tasks [][]int
 
 	if (len(os.Args) > 1) {
 		tCount, _ = strconv.ParseUint(os.Args[1], 10, 64)
@@ -60,39 +60,25 @@ func main() {
 	runtime.GOMAXPROCS(int(tCount))
 
 	fmt.Println("Start initializing...")
-	target := []int{3, 4, 1, 11, 8, 7, 2, 6, 5, 0, 9}
-	for i := 30000000; i > 12; i-- {
-		target = append(target, i)
-	}
-	target_copy := target
-	sortInstance = psort.New(target, tCount)
-	poolInstance = pool.New(tCount, psort.TSort, unsafe.Pointer(sortInstance))
+	tasks = createTasks()
 
-	start := time.Now()
-	poolInstance.Start();
-	fmt.Println("time=", time.Now().Sub(start))
+	for _, target := range tasks {
+		sortInstance = psort.New(target, tCount)
+		poolInstance = pool.New(tCount, psort.TSort, unsafe.Pointer(sortInstance))
+		poolInstance.ChangeTask(unsafe.Pointer(sortInstance))
 
-	result := make([]int, 0)
-	var sliceThreadSize float64 = float64(len(sortInstance.GetTarget())) / float64(tCount)
-	for i := uint64(0); i < tCount; i++ {
-		l := int(float64(i) * sliceThreadSize)
-		r := int(float64(i + 1) * sliceThreadSize)
-		result = psort.Merge(result, sortInstance.GetTarget()[l:r])
-	}
-	fmt.Println("final time=", time.Now().Sub(start))
-	fmt.Println(result[:100])
+		start := time.Now()
+		poolInstance.Start();
+		fmt.Println("time=", time.Now().Sub(start))
 
-	result = make([]int, 0)
-	sortInstance = psort.New(target_copy, tCount)
-	poolInstance.ChangeTask(unsafe.Pointer(sortInstance))
-	fmt.Println("new task started")
-	poolInstance.Start()
-	sliceThreadSize = float64(len(sortInstance.GetTarget())) / float64(tCount)
-	for i := uint64(0); i < tCount; i++ {
-		l := int(float64(i) * sliceThreadSize)
-		r := int(float64(i + 1) * sliceThreadSize)
-		result = psort.Merge(result, sortInstance.GetTarget()[l:r])
+		result := make([]int, 0)
+		var sliceThreadSize float64 = float64(len(sortInstance.GetTarget())) / float64(tCount)
+		for i := uint64(0); i < tCount; i++ {
+			l := int(float64(i) * sliceThreadSize)
+			r := int(float64(i + 1) * sliceThreadSize)
+			result = psort.Merge(result, sortInstance.GetTarget()[l:r])
+		}
+		fmt.Println("final time=", time.Now().Sub(start))
+		fmt.Println(result)
 	}
-	fmt.Println("final time=", time.Now().Sub(start))
-	fmt.Println(result[:100])
 }
